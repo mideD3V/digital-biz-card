b4568178f436e49761121a0ba8f3bb72c4152b18
mided3v <ayo4real6@gmail.com>
2023-01-04 10:16:37 +0100

    style: update

--- src/Components/about/About.jsx
+++ src/Components/about/About.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import './about.css'
 
 const About = () => {
   return (
-    <div>About</div>
+      <div id='about'>
+          <h3>About</h3>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequuntur ducimus reiciendis nostrum quod deleniti fugit est quae debitis quaerat, vitae ipsam accusamus fugiat, amet rem. Laborum voluptatem omnis placeat corporis.</p>
+    </div>
   )
 }
 
--- src/Components/header/Header.jsx
+++ src/Components/header/Header.jsx
@@ -1,21 +1,39 @@
 import React from 'react'
 import './header.css'
-import Mide from '../../asset/ayodev.png'
+import Mide from '../../asset/mide-dev.jpg'
 import {AiTwotoneMail} from 'react-icons/ai'
 import { AiOutlineLinkedin } from "react-icons/ai";
 
 const Header = () => {
   return (
-      <div>
-          <img src={Mide} alt="" />
-          <h2>Ayomide Oke</h2>
-          <p>Creative Front-End Developer</p>
+    <div className="header">
+      <img src={Mide} alt="" />
+      <h2>Ayomide Oke</h2>
+      <p id='description'>Creative Front-End Developer</p>
           <p className="location">Lagos, Nigeria</p>
-          <button> <span className="icon"> <AiTwotoneMail /> </span> Email</button>
           
-          <button> <span className="icon"> <AiOutlineLinkedin/> </span>LinkedIn</button>
+      <div className="socials_btn">
+        <button id='email_btn'>
+          {" "}
+          <span className="icon">
+            {" "}
+            <AiTwotoneMail />{" "}
+          </span>{" "}
+          Email
+        </button>
+
+        <button id='linkedin_btn'>
+          {" "}
+          <span className="icon">
+            {" "}
+            <AiOutlineLinkedin />{" "}
+          </span>
+          LinkedIn
+        </button>
+              
+      </div>
     </div>
-  )
+  );
 }
 
 export default Header
