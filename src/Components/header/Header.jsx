1f8593ce014e07b058a1545f81c41b733c117748
mided3v <ayo4real6@gmail.com>
2023-01-05 23:40:53 +0100

    Edited the header information

--- src/Components/header/Header.jsx
+++ src/Components/header/Header.jsx
@@ -1,39 +1,52 @@
-import React from 'react'
-import './header.css'
-import Mide from '../../asset/mide-dev.jpg'
-import {AiTwotoneMail} from 'react-icons/ai'
+import React from "react";
+import "./header.css";
+import Mide from "../../asset/mide-dev.jpg";
+import { AiTwotoneMail } from "react-icons/ai";
 import { AiOutlineLinkedin } from "react-icons/ai";
+import Date from '../../Components/date/Date'
 
 const Header = () => {
   return (
     <div className="header">
       <img src={Mide} alt="" />
+      <p>
+< Date/>
+      </p>
       <h2>Ayomide Oke</h2>
-      <p id='description'>Creative Front-End Developer</p>
-          <p className="location">Lagos, Nigeria</p>
-          
+      <p id="description">Creative Front-End Developer</p>
+      <p className="location">Lagos, Nigeria</p>
+
       <div className="socials_btn">
-        <button id='email_btn'>
+        {" "}
+        <a
+          href="mailto:ayomideoke9@gmail.com"
+          target="_blank"
+          rel="noopener noreferrer"
+          id="email_btn"
+        >
           {" "}
           <span className="icon">
             {" "}
             <AiTwotoneMail />{" "}
           </span>{" "}
           Email
-        </button> 
-
-        <button id='linkedin_btn'>
+        </a>{" "}
+        <a
+          href="https://www.linkedin.com/in/mid3/"
+          target="_blank"
+          rel="noopener noreferrer"
+          id="linkedin_btn"
+        >
           {" "}
           <span className="icon">
             {" "}
             <AiOutlineLinkedin />{" "}
           </span>
           LinkedIn
-        </button>
-              
+        </a>
       </div>
     </div>
   );
-}
+};
 
-export default Header
+export default Header;
