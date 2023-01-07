69176d9aec9232f8a3fb0f7fef15b4a927e20f17
mided3v <ayo4real6@gmail.com>
2023-01-07 18:59:11 +0100

    added social media icon

--- src/Components/footer/Footer.jsx
+++ src/Components/footer/Footer.jsx
@@ -3,27 +3,33 @@ import './footer.css'
 import { FaTwitter } from 'react-icons/fa'
 import { FaFacebookF } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
+import { AiOutlineInstagram } from "react-icons/ai";
 
 const Footer = () => {
   return (
     <div id="footer">
-          <ul>
-              <li>
-        <a href="https://wwww.twitter.com" className="twitter">
-          <FaTwitter />{" "}
-              </a></li>
-              <li>
-        <a href="https://wwww.facebook.com" className="facebook">
-          <FaFacebookF />{" "}
-              </a></li>
-              <li>
-        <a href="https://wwww.twitter.com" className="twitter">
-          <FaTwitter />{" "}
-              </a></li>
-              <li>
-        <a href="https://wwww.github.com.com" className="github">
-          <FaGithub />{" "}
-        </a></li>
+      <h5>Connect with Mide</h5>
+      <ul>
+        <li>
+          <a href="https://wwww.twitter.com" className="twitter">
+            <FaTwitter />{" "}
+          </a>
+        </li>
+        <li>
+          <a href="https://wwww.facebook.com" className="facebook">
+            <FaFacebookF />{" "}
+          </a>
+        </li>
+        <li>
+          <a href="https://wwww.twitter.com" className="instagram">
+            <AiOutlineInstagram />{" "}
+          </a>
+        </li>
+        <li>
+          <a href="https://wwww.github.com.com" className="github">
+            <FaGithub />{" "}
+          </a>
+        </li>
       </ul>
     </div>
   );
